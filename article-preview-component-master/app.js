95fca3ec33a329c0dc89689312b3ece53b3e0a05
gemar18 <92255011+gemar18@users.noreply.github.com>
2022-03-22 20:08:54 +0100

    Add files via upload

---
+++ article-preview-component-master/app.js
@@ -0,0 +1,8 @@
+const shareBtn=document.getElementById("btn-one");
+const shareBtn2=document.getElementById("btn-two");
+shareBtn.addEventListener("click",()=>{
+    share.classList.toggle("active")
+});
+shareBtn2.addEventListener("click",()=>{
+    share.classList.toggle("active")
+});
